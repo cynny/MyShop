d1796528456c4c9fea712d1f26f92c364b3822f6
cynny <99002017+cynny@users.noreply.github.com>
2022-03-09 22:39:44 -0600

    created header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 //arrow functions
 const App = () => {
   return (
     <>
+
+    <Header />
+
+    <main>
     <h1>Welcome to MyShop</h1>
+    </main>
+
+    <Footer />
+
     </>
+    
    );
 
 }
