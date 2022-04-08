dc5830652943453172c862c8fc229af14cfeba3c
cynny <99002017+cynny@users.noreply.github.com>
2022-04-07 21:08:45 -0500

    updated port for frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -16,4 +16,4 @@ const product = products.find((p) => p._id === req.params.id);
     res.json(product);
 })
 
-app.listen(3000, console.log('Server is running on port 3000'));
+app.listen(5000, console.log('Server is running on port 5000'));
